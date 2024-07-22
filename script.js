c8964ca4b114f719569dde65f5d62f2227e0a083
tarun-vasakam <157348103+tarun-vasakam@users.noreply.github.com>
2024-07-22 16:58:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function red1(){
+    let other=document.querySelectorAll(".opt-ans")
+    for (let i=0;i<other.length;i++)
+    {
+        other[i].style.backgroundColor="white"
+    }
+    let change=document.querySelector(".red1")
+    change.style.backgroundColor="crimson"
+}
+function red2(){
+    let other=document.querySelectorAll(".opt-ans")
+    for (let i=0;i<other.length;i++)
+    {
+        other[i].style.backgroundColor="white"
+    }
+    let change=document.querySelector(".red2")
+    change.style.backgroundColor="red"
+}
+function red3(){
+    let other=document.querySelectorAll(".opt-ans")
+    for (let i=0;i<other.length;i++)
+    {
+        other[i].style.backgroundColor="white"
+    }
+    let change=document.querySelector(".red3")
+    change.style.backgroundColor="red"
+}
+function green(){
+    let other=document.querySelectorAll(".opt-ans")
+    for (let i=0;i<other.length;i++)
+    {
+        other[i].style.backgroundColor="white"
+    }
+    let change=document.querySelector(".green")
+    change.style.backgroundColor="green"
+}
